3d95a19863b355a66a68dbb57fba83bbd7d2cd37
VishwashDhiman <vishwashdhiman@gmail.com>
2018-11-09 22:20:53 +0530

    Setting up redux and firebase

--- tour-trip-app/src/index.js
+++ tour-trip-app/src/index.js
@@ -7,6 +7,7 @@ import { createStore, applyMiddleware  } from 'redux'
 import rootReducer from './reducers/rootReducer'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import fbConfig from './config/fbConfig'
 
 const store = createStore(rootReducer,  applyMiddleware(thunk));
 
