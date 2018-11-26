5c1439a2b2740ce1589df5c1f0a1c64df58a8ecd
VishwashDhiman <vishwashdhiman@gmail.com>
2018-11-26 14:45:24 +0530

    Fixed Redirect bug

--- tour-trip-app/src/Component/Booking/TrainBooking.js
+++ tour-trip-app/src/Component/Booking/TrainBooking.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 class TrainBooking extends Component {
     render(){
