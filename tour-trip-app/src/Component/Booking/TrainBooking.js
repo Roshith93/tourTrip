c7f08ec04f28435c21d8bc02519017e1c7a93ce7
VishwashDhiman <vishwashdhiman@gmail.com>
2018-11-29 19:08:56 +0530

    Sending traindata of user to firebase

--- tour-trip-app/src/Component/Booking/TrainBooking.js
+++ tour-trip-app/src/Component/Booking/TrainBooking.js
@@ -1,19 +1,54 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import Chip from '@material-ui/core/Chip';
+import { userTrainData } from '../../reducers/actions/projectAction';
+import moment from 'moment';
+import Snackbar from '@material-ui/core/Snackbar';
+import Slide from '@material-ui/core/Slide';
 
-var available = [];
+function TransitionLeft(props) {
+    return <Slide style={{background: 'green'}} {...props} direction="left" />;
+}
 class TrainBooking extends Component {
-    // availableDays = () => {
-    //     console.log("asdad");
-    //     const { trainData } = this.props;
-    //     for(let j = 0; j < 7; j++)
-    //     if(trainData.days[j].runs === 'Y')
-    //         available.push(<Chip label={trainData.days[j].code} key={j} variant="outlined" />)
-        
-    //     console.log(available) 
-    // }
+    state = {
+        open: false,
+        load: false,
+        snackopen: false,
+        Transition: null,
+    };
+    userData = {
+        number: '',
+        travelsName: '',
+        source: '',
+        destination: '',
+        departureTime: '',
+        arrivalTime: '',
+        duration: '',
+        available: '',
+        id: '',
+        date: '',
+    }
+    handleClose = () => {
+        this.setState({ snackopen: false,
+            load: true });
+    }
+    handleSubmit = Transition => () => {
+        const { trainData, auth } = this.props;
+        let date = new Date();
+        this.userData.number = trainData.number;
+        this.userData.travelsName = trainData.travelsName;
+        this.userData.destination = trainData.destination;
+        this.userData.source = trainData.source;
+        this.userData.departureTime = trainData.departureTime;
+        this.userData.arrivalTime = trainData.arrivalTime;
+        this.userData.duration = trainData.duration;
+        // this.userData.available = trainData.available;
+        this.userData.date = moment(date).format('DD-MM-YYYY');
+        this.userData.id = auth.uid;
+        console.log(this.userData);
+        this.props.userTrainData(this.userData);
+        this.setState({ snackopen: true, Transition });
+    }
     render(){
         const { trainData, auth } = this.props;
         console.log(trainData); 
@@ -24,7 +59,7 @@ class TrainBooking extends Component {
                     <h4>Selected Train's Data</h4>
                     <div className="card-panel hoverable">
                         <pre>Train Number           :           {trainData.number}</pre>
-                        <pre>Train Name             :           {trainData.travelsName}</pre>
+                        <pre>Travelers Name         :           {trainData.travelsName}</pre>
                         <pre>Source Station         :           {trainData.source}</pre>
                         <pre>Destinaion Station     :           {trainData.destination}</pre>
                         <pre>Departure Time         :           {trainData.departureTime}</pre>
@@ -33,9 +68,19 @@ class TrainBooking extends Component {
                         <pre>Availabe Days          :           {trainData.available}</pre>
                     </div>
                     <div className="center">
-                        <button className="btn waves-effect waves-light" type="submit" name="action">Submit
+                        <button className="btn waves-effect waves-light" type="submit" name="action" onClick={this.handleSubmit(TransitionLeft)}>Submit
                             <i className="material-icons right">send</i>
                         </button>
+                        <Snackbar
+                            open={this.state.snackopen}
+                            onClose={this.handleClose}
+                            style={{backgroundColor: 'green'}}
+                            TransitionComponent={this.state.Transition}
+                            ContentProps={{
+                                'aria-describedby': 'message-id',
+                            }}
+                            message={<span id="message-id">Flights added successfully</span>}
+                        />
                     </div>
                 </div>
             </div>
@@ -49,4 +94,10 @@ const mapStateToProps = (state) => {
         auth: state.firebase.auth
     }
 }
-export default connect(mapStateToProps)(TrainBooking);
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        userTrainData: (data) => dispatch(userTrainData(data))
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(TrainBooking);
--- tour-trip-app/src/reducers/actions/projectAction.js
+++ tour-trip-app/src/reducers/actions/projectAction.js
@@ -26,6 +26,20 @@ export const userFlightData = (project) => {
   }
 };
 
+export const userTrainData = (project) => {
+  return (dispatch, getState, { getFirestore }) => {
+    // make async call to database
+    const firestore = getFirestore();
+    firestore.collection('userTrainData').add({
+      ...project
+    }).then(() => {
+      dispatch({ type: 'CREATE_PROJECT_SUCCESS' });
+    }).catch(err => {
+      dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+    });
+  }
+};
+
 export const addBusData = (busData) => {
   console.log(busData)
   return (dispatch, getState) => {
